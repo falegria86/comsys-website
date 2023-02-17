6a3c10bbd9b530406297cc793c17a7bf205bbb29
falegria2 <falegria@ittepic.edu.mx>
2023-02-16 19:15:49 -0700

    Se agregó favicon personalizado

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8694da29bedddf1"; }
+/******/ 		__webpack_require__.h = function() { return "74ead807e2057417"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.33fc9e14cf04131a.hot-update.js
+++ .next/static/webpack/webpack.33fc9e14cf04131a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9be6c5d86a87137c"; }
+/******/ 	__webpack_require__.h = function() { return "74ead807e2057417"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
